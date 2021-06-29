24b91269d1a3c4ef1ae236a5ff79cc3a49f32f87
Felixcrack <79382870+Felixcrack@users.noreply.github.com>
2021-06-28 19:31:42 -0500

    Initial commit

---
+++ src/help.js
@@ -0,0 +1,99 @@
+const help = (prefix) => {
+	return `⌜ *FX-BOT-73* ⌟  
+
+❍➣ *INFORMACION*
+
+ Comando : ⌜ ${prefix} ⌟
+Creador : Felixcrack
+Créditos Del Menu Shanduy
+
+❍➣ ¿Tutoriales de crear bots?
+Responde esta encuesta: 
+
+❍➣ *NUEVOS MENUS*
+ │⊷️ ${prefix}desmenu
+ │Descargar musica y videos de YT
+ │⊷️ ${prefix}version
+ │Conoce la versión de tu bot
+ │⊷️ ${prefix}welmenu
+ │⊷️ Comandos de bienvedia a grupos
+ ╰❏
+
+❍➣ *RESUELVE TUS DUDAS*
+ │
+ │⊷️ ${prefix}creador
+ │⊷️ Dudas o problemas aqui
+ ╰❏ 
+
+❍➣ *CREAR STICKERS*
+  │
+  │⊷️ ${prefix}sticker
+  │⊷️ ${prefix}attp
+  │⊷️  Crea texto de colores
+  │⊷️ ${prefix}emoji 
+  │⊷️  Convierte un emoji a sticker
+  │⊷️ ${prefix}stickergif
+  │⊷️ 6 segundos de video
+  ╰❏ 
+
+❍➣ *CONVERTIDORES*
+  │
+  │⊷️ ${prefix}toimg
+  │⊷️  De sticker a JPG
+  │⊷️ ${prefix}tomp3
+  │⊷️ De video a MP3
+  ╰❏ 
+
+❍➣ *AUDIO*
+  │
+  │⊷️ ${prefix}idioma
+  │⊷️ ${prefix}tts es (No Funcional)
+  ╰❏
+
+❍➣ *OTROS*
+  │
+  │⊷️ ${prefix}wame
+  │⊷️  Link de Whatsapp
+  │⊷️ ${prefix}qrcode
+  │⊷️ Coloca un texto
+  ╰❏ 
+
+❍➣ *GRUPOS*
+  │
+  │⊷️ ${prefix}closegc
+  │⊷️  Cerrar el grupo solo admins
+  │⊷️ ${prefix}opengc
+  │⊷️  Abrir grupo solo admins
+  │⊷️ ${prefix}kick o pafuera
+  │⊷️  Eliminar a un miembro 
+  │⊷️ ${prefix}promote
+  │⊷️  Dar admin a un miembro
+  │⊷️ ${prefix}demote
+  │⊷️  Quitar el admin
+  │⊷️ ${prefix}listadmin
+  │⊷️  Ver a todos los admins
+  │⊷️ ${prefix}hidetag
+  │⊷️  Menciona a todos en secreto
+  │⊷️ ${prefix}linkgc
+  │⊷️  Link del grupo
+  │⊷️ ${prefix}tagall
+  │⊷️ Nombra a todos los del grupo
+  ╰❏ 
+
+❏ Para usar estas funciones el bot necesita admin
+  
+❍➣ *NSWF* 
+  │⊷️ ${prefix}nsfwmenu
+  ╰❏
+
+❏ Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
+
+❍➣ *CRÉDITOS*
+  │ Felixcrack : https://bit.ly/Felixcrack-409-YT
+  │ Shanduy : http://bit.ly/Shandñuy
+  │ ÆZT : http://bit.ly/ÆZT
+  ╰❏     
+`
+}
+
+exports.help = help
